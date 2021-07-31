92f0a1079cf9a25a002dd21f7f3dd361d9c9d8c5
ZHANG Yi <45352743+PanoAsh@users.noreply.github.com>
2021-07-31 10:53:16 +0200

    Add files via upload

--- video_to_frames.py
+++ video_to_frames.py
@@ -2,20 +2,21 @@ import cv2
 import os
 
 
-def Vid2KeyFrm():
-    seq = '_-0cfJOmUaNNI_1'  # par example
-    seq_path = os.path.join(os.getcwd(), seq + '.mp4')  # please take care of your own path
+def Vid2Frm():
+    seqs = os.listdir(os.path.join(os.getcwd(), 'Videos'))  # please take care of your own path
+    for seq in seqs:
+        seq_path = os.path.join(os.getcwd(), 'Videos', seq)
+        save_path = os.path.join(os.getcwd(), 'Frames', seq[:-4])
+        if not os.path.exists(save_path): os.makedirs(save_path)
+        cap = cv2.VideoCapture(seq_path)
+        frames_num = int(cap.get(7))
+        countF = 0
+        for i in range(frames_num):
+            ret, frame = cap.read()
+            cv2.imwrite(os.path.join(save_path, format(str(countF), '0>5s') + '.png'), frame)
+            print(" {} frames are extracted.".format(countF))
+            countF += 1
 
-    save_path = os.path.join(os.getcwd(), seq)
-    if not os.path.exists(save_path): os.makedirs(save_path)
-    cap = cv2.VideoCapture(seq_path)
-    frames_num = int(cap.get(7))
-    countF = 0
-    for i in range(frames_num):
-        ret, frame = cap.read()
-        cv2.imwrite(os.path.join(save_path, format(str(countF), '0>5s') + '.png'), frame)
-        print(" {} frames are extracted.".format(countF))
-        countF += 1
 
 if __name__ == '__main__':
-    Vid2KeyFrm()
+    Vid2Frm()
